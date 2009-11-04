01e3bca8e38855fc644912f5a31a0c31b1ba7fb0
Coda Hale <coda.hale@gmail.com>
2009-11-04 11:08:34 -0800

    Added a start gate to the LamportClock test.
    On a fast machine, a single thread could conceivably handle all
    X tasks serially instead of banging on the concurrency door like
    the test needs to do.

--- src/test/scala/com/codahale/vlock/specs/LamportClockSpec.scala
+++ src/test/scala/com/codahale/vlock/specs/LamportClockSpec.scala
@@ -5,18 +5,20 @@ import com.codahale.vlock.{LamportClock, Timestamp}
 import org.scalatest.Spec
 import org.scalatest.matchers.MustMatchers
 
-import java.util.concurrent.{Executors, TimeUnit}
+import java.util.concurrent.{Executors, TimeUnit, CountDownLatch}
 
 object ConcreteLamportClock extends LamportClock
 
 class LamportClockSpec extends Spec with MustMatchers {
   describe("a Lamport clock being accessed across many threads") {
     it("returns strictly-increasing integers") {
+      val startGate = new CountDownLatch(1)
       val pool = Executors.newFixedThreadPool(20)
-      for (i <- 0 to 20) {
+      for (i <- 0 to 40) {
         pool.submit(new Runnable() {
           def run {
             var timestamps = List[Timestamp]()
+            startGate.await
             for (j <- 0 to 1000) {
               timestamps = ConcreteLamportClock.timestamp :: timestamps
             }
@@ -24,7 +26,7 @@ class LamportClockSpec extends Spec with MustMatchers {
           }
         })
       }
-      
+      startGate.countDown
       pool.shutdown()
       pool.awaitTermination(30, TimeUnit.SECONDS)
     }
